290bee29aac5a1d77acbdb6f2202b5fa15ef727c
Anson He <ansonhe1997@gmail.com>
2020-10-19 16:01:31 +0800

    Added WT Web Bluetooth Examples

---
+++ examples/WioTerminal_WebBluetooth/WioTerminal_WebBluetooth.ino
@@ -0,0 +1,118 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <Seeed_erpcUnified.h>
+#include <LIS3DHTR.h>
+
+#define accelerometerService "19b10000-e8f2-537e-4f6c-d104768a1214"
+#define firstCharacteristic  "19b10010-e8f2-537e-4f6c-d104768a1214"
+#define DESCRIPTOR_UUID      "19b10010"
+
+LIS3DHTR<TwoWire> lis;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+BLEServer *pServer = NULL;
+BLECharacteristic * pCharacteristic;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      Serial.println("MyServerCallbacks onConnect ");
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        for (int i = 0; i < rxValue.length(); i++)
+          Serial.print(rxValue[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+//  while(!Serial){};
+  
+  lis.begin(Wire1);
+  if (!lis) {
+    Serial.println("Accelerater Error!");
+    while(1);
+  }
+  lis.setOutputDataRate(LIS3DHTR_DATARATE_25HZ); //Data output rate
+  lis.setFullScaleRange(LIS3DHTR_RANGE_2G); //Scale range set to 2g
+  Serial.println("Accelerater Initialised!");
+  
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("Accelerometer");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  
+  BLEService *pService = pServer->createService(accelerometerService);
+  pCharacteristic = pService->createCharacteristic(
+                                         firstCharacteristic,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+  pCharacteristic->setAccessPermissions(GATT_PERM_READ | GATT_PERM_WRITE);
+  BLEDescriptor *pDescriptor = pCharacteristic->createDescriptor(
+                                         DESCRIPTOR_UUID,
+                                          ATTRIB_FLAG_VOID | ATTRIB_FLAG_ASCII_Z,
+                                         GATT_PERM_READ | GATT_PERM_WRITE,
+                                         2
+                                         );
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(accelerometerService);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() { 
+  if (deviceConnected) {
+    updateAcceleration();
+  }
+      // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+}
+
+void updateAcceleration() {
+  float x_values, y_values, z_values;
+  x_values = lis.getAccelerationX();
+  y_values = lis.getAccelerationY();
+  z_values = lis.getAccelerationZ();
+
+  String accelerometerData = String(x_values)+"|"+String(y_values)+"|"+String(z_values);
+  Serial.println(accelerometerData);
+  pCharacteristic->setValue(accelerometerData.c_str());
+  pCharacteristic->notify();
+  delay(20);
+}
